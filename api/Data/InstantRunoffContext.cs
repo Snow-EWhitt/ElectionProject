f60bee59916fe91a06b25f3064c19f142b9d4c79
Snow-EWhitt <ethanwhittaker02@gmail.com>
2022-09-14 14:20:56 -0500

    Initial Commit

--- api/Data/InstantRunoffContext.cs
+++ api/Data/InstantRunoffContext.cs
@@ -32,15 +32,6 @@ namespace election.Data
         public virtual DbSet<Veffectiveballot> Veffectiveballots { get; set; } = null!;
         public virtual DbSet<Vresultspivot> Vresultspivots { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("host=192.168.1.3; database=iro; user id=iro; password=Secret123");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Ballot>(entity =>
