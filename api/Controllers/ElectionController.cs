327bfe31284cfebc2a5157064b5dcd01f8c68e0c
Snow-EWhitt <ethanwhittaker02@gmail.com>
2022-09-22 09:00:52 -0500

    Fixed URL address and retyped ElectionController.

---
+++ api/Controllers/ElectionController.cs
@@ -0,0 +1,25 @@
+﻿using election.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace election.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElectionController : ControllerBase
+    {
+        private readonly InstantRunoffContext context;
+
+        public ElectionController(InstantRunoffContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{electionId}/isopen")]
+        public async Task<string> IsOpen(int electionId)
+        {
+            var row = await context.Elections.FirstOrDefaultAsync(e => e.Id == electionId);
+            return row.Ballotingclosed ? "false" : "true";
+        }
+    }
+}
